6cbe7cabf917844f8b118e5feedc6577f7fce241
santiifm <flecha.mrc@hotmail.com>
2024-06-23 19:53:54 -0300

    add currency check to model

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class Transaction < ApplicationRecord
+  ALLOWED_CURRENCIES = ['USD', 'BTC'].freeze
   belongs_to :client
+  validates :source_currency, :target_currency, inclusion: { in: ALLOWED_CURRENCIES, message: '%{value} is not a valid currency' }
   validates :source_currency, :source_amount, :target_currency, :target_amount, :client_id, :exchange_rate, presence: true
 end
